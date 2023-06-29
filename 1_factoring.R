19692724a031e9536dbc130b485f8ba2002565dd
AnceTetere <ance.tetere@outlook.com>
2023-06-29 16:31:18 +0300

    Factoring the first data set

---
+++ 1_factoring.R
@@ -0,0 +1,19 @@
+getwd()
+setwd("D:\\R scripts and databases\\Data sets\\UDEMY_R_programmingCourse_Section6" )
+getwd()
+
+movies <- read.csv("P2-Movie-Ratings.csv")
+head(movies)
+colnames(movies) <- c("Film", "Genre", "CriticRating", "AudienceRating",
+                      "BudgetMillions", "Year")
+tail(movies)
+str(movies)
+
+movies$Genre <- factor(movies$Genre)
+str(movies$Genre)
+
+summary(movies)
+movies$Year <- factor(movies$Year)
+
+summary(movies)
+str(movies)
